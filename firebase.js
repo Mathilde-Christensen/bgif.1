e1fd52c17938b35175b81c89623968d881ec0972
Mathilde-Christensen <mathildemc27@gmail.com>
2026-04-29 11:12:33 +0200

    mere database

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZtt1vxZN99yh1rCiXMXFHKNRFcvCXpuE",
+  authDomain: "bgif-b18a5.firebaseapp.com",
+  databaseURL: "https://bgif-b18a5-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bgif-b18a5",
+  storageBucket: "bgif-b18a5.firebasestorage.app",
+  messagingSenderId: "546874216542",
+  appId: "1:546874216542:web:14166d77d29c77ec6b24cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
