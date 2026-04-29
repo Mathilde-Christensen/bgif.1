b6ce97f673dfa66f7db3120994849f44dbac6023
Mathilde-Christensen <mathildemc27@gmail.com>
2026-04-29 16:12:23 +0200

    fikse fejl

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 import './assets/main.scss'
 
-
-createApp(App).mount('#app')
-
+import { createApp } from 'vue'
+import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
-
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,3 @@
-import Frivilligside from "@/views/Frivilligside.vue";
-import Footer from "@/components/Footer.vue";
 import { createRouter, createWebHistory } from 'vue-router'
 import Frivilligside from '@/views/Frivilligside.vue'
 
@@ -9,14 +7,9 @@ const router = createRouter({
     {
       path: '/',
       name: 'frivilligside',
-      component: Frivilligside
+      component: Frivilligside,
     },
-    {
-        path: '/footer',
-        name: 'footer',
-        component: Footer
-      },
-  ]
+  ],
 })
 
 export default router
