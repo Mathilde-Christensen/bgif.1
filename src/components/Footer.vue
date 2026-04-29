ccda24f626881baddbed94b27b109c63e789b282
k1rja <kirjabanke1999@gmail.com>
2026-04-29 15:22:49 +0200

    footer tilføjet

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<script setup>
+ import facebookIcon from '../assets/images/icons/facebook_logo_secondary.webp'
+ import instagramIcon from '../assets/images/icons/instagram_glyph_white.webp' 
+</script>
+
+<template>
+    <footer class>
+
+    </footer>
+</template>
