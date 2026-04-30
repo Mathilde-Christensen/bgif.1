cb57e25afe03c01c50180c35e5db291ff9d7d3a0
Mathilde-Christensen <mathildemc27@gmail.com>
2026-04-30 10:50:17 +0200

    Header

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Frivilligside from '@/views/Frivilligside.vue'
+import Calendar from '@/views/Calendar.vue'
+import Profil from '@/views/Profil.vue'
+import Chat from '@/views/Chat.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,21 @@ const router = createRouter({
       path: '/',
       name: 'frivilligside',
       component: Frivilligside,
+    },
+    {
+      path: '/calendar',
+      name: 'calendar',
+      component: Calendar,
+    },
+    {
+      path: '/profil',
+      name: 'profil',
+      component: Profil,
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: Chat,
     },
   ],
 })
