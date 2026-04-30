4a39bb69133f494744b3943520538541bd31a2d5
Mathilde-Christensen <mathildemc27@gmail.com>
2026-04-30 11:37:43 +0200

    Menu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import { RouterView } from 'vue-router'
   import Footer from './components/Footer.vue'
+  import Header from './components/Header.vue'
 </script>
 
 <template>
+  <Header />
   <RouterView />
   <Footer />
 </template>
